1f5e508541e61926ad1ca7df294fe3e5d4b819da
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-10-24 14:16:05 -0400

    Initial Commit

---
+++ emojiscroll/ContentView.swift
@@ -0,0 +1,26 @@
+//
+/**
+ * ContentView.swift
+ * emojiscroll
+ *
+ * Created by Nick Spreen (spreen.co) on 10/24/25.
+ * 
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emojiscroll/emojiscrollApp.swift
@@ -0,0 +1,19 @@
+//
+/**
+ * emojiscrollApp.swift
+ * emojiscroll
+ *
+ * Created by Nick Spreen (spreen.co) on 10/24/25.
+ * 
+ */
+
+import SwiftUI
+
+@main
+struct emojiscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
